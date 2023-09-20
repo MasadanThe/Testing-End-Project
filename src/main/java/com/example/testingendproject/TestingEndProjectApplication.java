40991c0683cfdca2c207a28f41959678a56277fb
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-20 13:50:06 +0200

    Initialized project

---
+++ src/main/java/com/example/testingendproject/TestingEndProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingEndProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingEndProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testingendproject/TestingEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingEndProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
