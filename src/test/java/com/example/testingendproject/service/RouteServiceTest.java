6d5464e3052ac87dce08917f8c3cd234cc83dd23
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 00:09:56 +0200

    Test that fails because of trying new thing

---
+++ src/test/java/com/example/testingendproject/service/RouteServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.testingendproject.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RouteServiceTest {
+
+    @Test
+    void updateSale() {
+    }
+
+    @Test
+    void getRoutes() {
+        assertEquals(5,3);
+    }
+
+    @Test
+    void createBookingSupplier() {
+    }
+}
