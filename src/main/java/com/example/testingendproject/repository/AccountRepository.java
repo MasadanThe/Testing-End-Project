b27a2f57ff4a1986faf64b9a382a36dd62037c01
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:51:03 +0200

    Made the repository and interface that extend JpaRepository

--- src/main/java/com/example/testingendproject/repository/AccountRepository.java
+++ src/main/java/com/example/testingendproject/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package com.example.testingendproject.repository;
 
+import com.example.testingendproject.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AccountRepository {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
