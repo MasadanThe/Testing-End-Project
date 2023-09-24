1c2e6ea46f71f074b3f08f179fa0ab2f47f2339e
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:42:47 +0200

    Added the basic crut operations for accounts

--- src/main/java/com/example/testingendproject/service/AccountService.java
+++ src/main/java/com/example/testingendproject/service/AccountService.java
@@ -1,7 +1,28 @@
 package com.example.testingendproject.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class AccountService {
+    public void createAccount(){
+
+    }
+
+    public void updateAccount(){
+
+    }
+
+    public void deleteAccount(){
+
+    }
+
+    public void addBooking(){
+
+    }
+
+    public void deleteBooking(){
+
+    }
 }
