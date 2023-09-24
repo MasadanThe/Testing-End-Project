fd694e23ab70c77b2da9805d0edb4e6d82c934cc
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:54:18 +0200

    Made the repository an interface that extends JpaRepository

--- src/main/java/com/example/testingendproject/repository/RouteRepository.java
+++ src/main/java/com/example/testingendproject/repository/RouteRepository.java
@@ -1,7 +1,9 @@
 package com.example.testingendproject.repository;
 
+import com.example.testingendproject.model.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class RouteRepository {
+public interface RouteRepository extends JpaRepository<Route, Long> {
 }
