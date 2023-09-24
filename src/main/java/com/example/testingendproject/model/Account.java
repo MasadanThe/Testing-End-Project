8473525ebac86c9166b5e2c6d0747d93c9d765a4
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:28:51 +0200

    Added annotations

--- src/main/java/com/example/testingendproject/model/Account.java
+++ src/main/java/com/example/testingendproject/model/Account.java
@@ -1,4 +1,15 @@
 package com.example.testingendproject.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Account {
 }
