f1b9d888c91ea16a7401eb62639a73b337ac081f
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:14:45 +0200

    Added PostMapping for creae_account

--- src/main/java/com/example/testingendproject/controller/ApiController.java
+++ src/main/java/com/example/testingendproject/controller/ApiController.java
@@ -1,8 +1,14 @@
 package com.example.testingendproject.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+    @PostMapping("create_account")
+    public ResponseEntity<String> createAccount(){
+        return ResponseEntity.ok("ok");
+    }
 }
