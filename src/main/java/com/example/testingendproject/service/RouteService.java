2074ef69292ede9667e839f0484c0af1457ec438
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:45:23 +0200

    Added empty basic methods for RouteService

--- src/main/java/com/example/testingendproject/service/RouteService.java
+++ src/main/java/com/example/testingendproject/service/RouteService.java
@@ -1,7 +1,22 @@
 package com.example.testingendproject.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class RouteService {
+
+    public void updateSale(){
+
+    }
+
+    public void getRoutes(){
+
+    }
+
+    public void createBookingSupplier(){
+
+    }
 }
