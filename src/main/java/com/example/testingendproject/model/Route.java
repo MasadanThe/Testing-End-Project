80132203fbab84af97b86044da78031917c6151b
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-24 21:35:34 +0200

    Added id value

--- src/main/java/com/example/testingendproject/model/Route.java
+++ src/main/java/com/example/testingendproject/model/Route.java
@@ -1,6 +1,9 @@
 package com.example.testingendproject.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +15,7 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // AUTO_INCREMENT
+    private long id;
 }
