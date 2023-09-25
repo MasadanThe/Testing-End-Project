867cbc5a3f6e0a9ae9b3edf3a866d0716184541d
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 00:42:49 +0200

    Implemented the test method confirmVerifyJwtToken

--- src/test/java/com/example/testingendproject/service/JwtServiceTest.java
+++ src/test/java/com/example/testingendproject/service/JwtServiceTest.java
@@ -18,7 +18,8 @@ class JwtServiceTest {
 
     @Test
     void confirmVerifyJwtToken() {
-
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QifQ.4g0fpgEiD5n7PpYUkFbYBzwP7bKJ7K8vlYDiIVJMTR8";
+        assert(jwtService.verifyJwtToken("4gthuyi9er3agdjsskjdlffdkl"));
 
     }
 }
