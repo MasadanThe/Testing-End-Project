b64327311539886d49404bc719e5376bb6ed22f8
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 01:05:00 +0200

    Created mock for verifyAuthentication

--- src/test/java/com/example/testingendproject/service/AuthServiceImplTest.java
+++ src/test/java/com/example/testingendproject/service/AuthServiceImplTest.java
@@ -1,16 +1,22 @@
 package com.example.testingendproject.service;
 
 import com.example.testingendproject.model.Account;
+import com.example.testingendproject.model.Route;
+import com.example.testingendproject.repository.AccountRepository;
 import com.example.testingendproject.repository.RouteRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
@@ -22,6 +28,15 @@ class AuthServiceImplTest {
     AccountService accountService;
     @Test
     void verifyAuthentication() {
-        assertEquals(false, "MrCool");
+        var account1 = Account.builder()
+                .username("Troll")
+                .accountType("ADMIN")
+                .contactInformation("8973045653")
+                .paymentInformation("435252432")
+                .paymentHistory("")
+                .activeBookings("").build();
+        when(accountService.findByUsername("Troll")).thenReturn(account1);
+        authService.authenticate("Troll");
+        verify(accountService, times(2)).findByUsername(any());
     }
 }
