b58c8cac0af2701cc139fd4b0f5c093c30485f48
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 00:24:47 +0200

    Added authService to JwtService

--- src/main/java/com/example/testingendproject/service/JwtService.java
+++ src/main/java/com/example/testingendproject/service/JwtService.java
@@ -1,7 +1,11 @@
 package com.example.testingendproject.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    @Autowired(required = false)
+    AuthService authService;
+
 }
