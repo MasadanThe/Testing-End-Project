d10a4ba8dd543555661d21c09bd256255a69b759
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 18:48:51 +0200

    Added when and verify to verifyThatReturnedListIsNotNull

--- src/test/java/com/example/testingendproject/service/RouteServiceTest.java
+++ src/test/java/com/example/testingendproject/service/RouteServiceTest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RouteServiceTest {
@@ -43,6 +44,9 @@ class RouteServiceTest {
         List<Route> routeList = new ArrayList<Route>();
         routeList.add(route);
 
+        when(routeService.getRoutesFromSupplier1().thenReturn(routeList));
+
+        verify(routeService, times(1)).getRoutes();
     }
 
     @Test
