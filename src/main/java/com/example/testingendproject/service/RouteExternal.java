ec8337b47dceb09bf89d51b32d97d89a06be65eb
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 18:59:03 +0200

    Service annotation for RouteExternal

--- src/main/java/com/example/testingendproject/service/RouteExternal.java
+++ src/main/java/com/example/testingendproject/service/RouteExternal.java
@@ -1,9 +1,11 @@
 package com.example.testingendproject.service;
 
 import com.example.testingendproject.model.Route;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface RouteExternal {
     public List<Route> getRoutesFromSupplier1();
     public List<Route> getRoutesFromSupplier2();
