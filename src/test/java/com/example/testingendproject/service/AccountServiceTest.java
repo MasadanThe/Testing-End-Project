2ba960f9238dd3b7a026d0512e813b49846b3f0f
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 21:04:55 +0200

    Added AccountServiceTest

---
+++ src/test/java/com/example/testingendproject/service/AccountServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.testingendproject.service;
+
+import com.example.testingendproject.model.Route;
+import com.example.testingendproject.repository.RouteRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class AccountServiceTest {
+
+    @Autowired
+    AccountService AccountService;
+
+}
