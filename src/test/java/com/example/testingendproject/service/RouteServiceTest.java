8896e8701d42615ff1eaeeaa10b6cfb143da2313
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 18:30:47 +0200

    Made routeService be of the implemented one

--- src/test/java/com/example/testingendproject/service/RouteServiceTest.java
+++ src/test/java/com/example/testingendproject/service/RouteServiceTest.java
@@ -3,6 +3,7 @@ package com.example.testingendproject.service;
 import com.example.testingendproject.repository.RouteRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class RouteServiceTest {
 
-    @Mock
-    private RouteService routeService;
+    @InjectMocks
+    private RouteService routeService = new RouteServiceImpl();
 
     @Mock
     RouteRepository routeRepository;
