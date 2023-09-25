6ba62cd371d8fb984bb6d9864780e060481e0b11
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 01:55:38 +0200

    Added ApiControllerTest

---
+++ src/test/java/com/example/testingendproject/controller/ApiControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.testingendproject.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiControllerTest {
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void updateAccount() {
+    }
+
+    @Test
+    void deleteAccount() {
+    }
+
+    @Test
+    void addBooking() {
+    }
+
+    @Test
+    void deleteBooking() {
+    }
+
+    @Test
+    void updateSale() {
+    }
+
+    @Test
+    void getRoutes() {
+    }
+
+    @Test
+    void createBookingSupplier() {
+    }
+}
