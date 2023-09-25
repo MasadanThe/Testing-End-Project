3cdbec8200a093bd373da7f21f1788b4b6483dab
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 01:01:09 +0200

    Changed AuthServiceImplTest to mockito

--- src/test/java/com/example/testingendproject/service/AuthServiceImplTest.java
+++ src/test/java/com/example/testingendproject/service/AuthServiceImplTest.java
@@ -1,17 +1,27 @@
 package com.example.testingendproject.service;
 
+import com.example.testingendproject.model.Account;
+import com.example.testingendproject.repository.RouteRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
 
-    @Autowired
-    AuthService authService;
+    @InjectMocks
+    private AuthService authService = new AuthServiceImpl();
+
+    @Mock
+    AccountService accountService;
     @Test
-    void authenticate() {
+    void verifyAuthentication() {
+        assertEquals(false, "MrCool");
     }
 }
