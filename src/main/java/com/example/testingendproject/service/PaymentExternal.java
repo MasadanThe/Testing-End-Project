3d7a5d7abc8454cb2674984c5759522c3709b3fa
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 21:59:33 +0200

    Added PaymentExternal

---
+++ src/main/java/com/example/testingendproject/service/PaymentExternal.java
@@ -0,0 +1,8 @@
+package com.example.testingendproject.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface PaymentExternal {
+    public String checkPayment();
+}
