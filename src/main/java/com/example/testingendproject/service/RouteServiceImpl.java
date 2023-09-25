4d2d1451e97ca46f5d1500c1799d66724860200c
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 18:29:05 +0200

    Made RouteServiceImpl implement RouteService

--- src/main/java/com/example/testingendproject/service/RouteServiceImpl.java
+++ src/main/java/com/example/testingendproject/service/RouteServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service("routeService")
-public class RouteServiceImpl {
+public class RouteServiceImpl implements RouteService{
 
     public void updateSale(){
 
