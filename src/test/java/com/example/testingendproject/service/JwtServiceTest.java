348656a34f5cec7a34cd3ba06c3d56ace2e1710a
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 00:29:02 +0200

    Added JwtServiceTest

---
+++ src/test/java/com/example/testingendproject/service/JwtServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.testingendproject.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class JwtServiceTest {
+    @Autowired
+    JwtService jwtService;
+
+    @Test
+    void getJwtToken() {
+    }
+}
