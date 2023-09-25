2293a47b934512221d6ed010648f4779f73a1336
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 20:16:58 +0200

    Added findById in reouteRepository

--- src/main/java/com/example/testingendproject/repository/RouteRepository.java
+++ src/main/java/com/example/testingendproject/repository/RouteRepository.java
@@ -4,7 +4,10 @@ import com.example.testingendproject.model.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
     Route findAllByContractor(String contractor);
+    Optional<Route> findById(Long id);
 }
