cd2e9628fe103716ae2aaa4d9014627d48537092
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 19:21:31 +0200

    Removed getRoutesFromSupplier4

--- src/main/java/com/example/testingendproject/service/RouteExternal.java
+++ src/main/java/com/example/testingendproject/service/RouteExternal.java
@@ -10,5 +10,4 @@ public interface RouteExternal {
     public List<Route> getRoutesFromSupplier1();
     public List<Route> getRoutesFromSupplier2();
     public List<Route> getRoutesFromSupplier3();
-    public List<Route> getRoutesFromSupplier4();
 }
