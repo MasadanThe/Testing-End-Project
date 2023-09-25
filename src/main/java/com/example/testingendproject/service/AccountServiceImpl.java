07fc6f01bed4be78730ad5e0ecbbbdd23289d7de
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 21:20:48 +0200

    Added accountRepository to accountService

--- src/main/java/com/example/testingendproject/service/AccountServiceImpl.java
+++ src/main/java/com/example/testingendproject/service/AccountServiceImpl.java
@@ -1,12 +1,16 @@
 package com.example.testingendproject.service;
 
 import com.example.testingendproject.model.Account;
+import com.example.testingendproject.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AccountServiceImpl {
+    @Autowired
+    AccountRepository accountRepository;
     public void createAccount(Account account){
 
     }
