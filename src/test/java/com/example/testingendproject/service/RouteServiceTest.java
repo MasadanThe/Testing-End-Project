2bbb4950bc6bddd30da6268376e296cd60b9e4c5
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 18:24:48 +0200

    Changed to mockito

--- src/test/java/com/example/testingendproject/service/RouteServiceTest.java
+++ src/test/java/com/example/testingendproject/service/RouteServiceTest.java
@@ -1,15 +1,18 @@
 package com.example.testingendproject.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class RouteServiceTest {
 
-    @Autowired
+    @Mock
     private RouteService routeService;
     @Test
     void updateSale() {
