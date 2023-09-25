240481e0d53dc8963d7e9c6e63e5ed3344c3bef1
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 01:56:24 +0200

    Added annotations to ApiControllerTest

--- src/test/java/com/example/testingendproject/controller/ApiControllerTest.java
+++ src/test/java/com/example/testingendproject/controller/ApiControllerTest.java
@@ -1,10 +1,14 @@
 package com.example.testingendproject.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 class ApiControllerTest {
+
 
     @Test
     void createAccount() {
