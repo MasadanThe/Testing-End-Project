b7b0fb2b6758bd556e70949325cae432a1b755c0
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 00:27:38 +0200

    Created empty method getJwtToken in JwtService

--- src/main/java/com/example/testingendproject/service/JwtService.java
+++ src/main/java/com/example/testingendproject/service/JwtService.java
@@ -2,10 +2,14 @@ package com.example.testingendproject.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.auth0.jwt.JWT;
 
 @Service
 public class JwtService {
     @Autowired(required = false)
     AuthService authService;
 
+    public String getJwtToken(String username){
+        return null;
+    }
 }
