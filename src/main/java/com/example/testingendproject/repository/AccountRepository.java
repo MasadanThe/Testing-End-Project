7bd8ab58eb4f41e994592ed9d2385f581476da5d
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 22:08:32 +0200

    Added findByUsername to AccountRepository

--- src/main/java/com/example/testingendproject/repository/AccountRepository.java
+++ src/main/java/com/example/testingendproject/repository/AccountRepository.java
@@ -1,9 +1,13 @@
 package com.example.testingendproject.repository;
 
 import com.example.testingendproject.model.Account;
+import com.example.testingendproject.model.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
 }
