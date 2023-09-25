daf093cd65cefc78054c1cea3674783636903826
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 00:41:33 +0200

    Created test confirmVerifyJwtToken

--- src/test/java/com/example/testingendproject/service/JwtServiceTest.java
+++ src/test/java/com/example/testingendproject/service/JwtServiceTest.java
@@ -15,4 +15,10 @@ class JwtServiceTest {
 
         assertEquals("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QifQ.4g0fpgEiD5n7PpYUkFbYBzwP7bKJ7K8vlYDiIVJMTR8",jwtService.getJwtToken("test"));
     }
+
+    @Test
+    void confirmVerifyJwtToken() {
+
+
+    }
 }
