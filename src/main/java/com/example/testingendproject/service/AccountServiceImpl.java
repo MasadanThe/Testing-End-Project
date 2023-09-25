77e0a0d62a6d0224da53da11e8f0077a086523d7
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 21:40:13 +0200

    Implemented updateAccount()

--- src/main/java/com/example/testingendproject/service/AccountServiceImpl.java
+++ src/main/java/com/example/testingendproject/service/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.testingendproject.service;
 
 import com.example.testingendproject.model.Account;
+import com.example.testingendproject.model.Route;
 import com.example.testingendproject.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,23 @@ public class AccountServiceImpl implements AccountService{
     }
 
     public void updateAccount(Long id, Account account){
+        //Gets all accounts and find the one by id
+        //Had problems finding by id in JPA that's why it is done manually
+        List<Account> accountList = getAccounts();
+        Account foundAccount = new Account();
+        for (Account account1: accountList) {
+            if (account1.getId() == id){
+                foundAccount = account1;
+            }
 
+        }
+        //Updates the information
+        foundAccount.setUsername(account.getUsername());
+        foundAccount.setActiveBookings(account.getActiveBookings());
+        foundAccount.setContactInformation(account.getContactInformation());
+        foundAccount.setPaymentHistory(account.getPaymentHistory());
+        foundAccount.setPaymentInformation(account.getPaymentInformation());
+        createAccount(foundAccount);
     }
 
     public void deleteAccount(){
