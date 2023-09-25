3d8de0e2d821026e80bea2986a6da05cb668bad6
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-25 22:03:22 +0200

    Added AccountServiceTestMockito

---
+++ src/test/java/com/example/testingendproject/service/AccountServiceTestMockito.java
@@ -0,0 +1,31 @@
+package com.example.testingendproject.service;
+
+import com.example.testingendproject.model.Route;
+import com.example.testingendproject.repository.AccountRepository;
+import com.example.testingendproject.repository.RouteRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTestMockito {
+
+    @InjectMocks
+    private AccountService accountService  = new AccountServiceImpl();
+
+    @Mock
+    AccountRepository accountRepository;
+
+    @Mock PaymentExternal paymentExternal;
+
+
+
+}
