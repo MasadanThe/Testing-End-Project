722240525c173f61a90f9d59fc07f5887bc756d1
MasadanThe <MasadanThe@users.noreply.github.com>
2023-09-26 17:37:48 +0200

    deleteBooking() is now red as expected

--- src/test/java/com/example/testingendproject/controller/ApiControllerTest.java
+++ src/test/java/com/example/testingendproject/controller/ApiControllerTest.java
@@ -34,7 +34,7 @@ class ApiControllerTest {
     @BeforeAll
     static void addAccount(@Autowired AccountService accountService) {
         boolean firstAccount = accountService.createAccount(new Account("Test56", "435534534", "432243342", "2", "3", "User"));
-        boolean secondAccount = accountService.createAccount(new Account("Test24", "435534534", "432243342", "2", "3", "User"));
+        boolean secondAccount = accountService.createAccount(new Account("Test24", "435534534", "432243342", "2", "5,3", "User"));
         assertEquals(true, firstAccount);
         assertEquals(true, secondAccount);
     }
@@ -84,7 +84,7 @@ class ApiControllerTest {
 
 
     void testEndToEndUpdateAccount() throws Exception {
-        Account newAccount = new Account("Test24", "86778876", "7688678", "2", "3", "User");
+        Account newAccount = new Account("Test24", "86778876", "7688678", "2", "5,3", "User");
         //Adds a new user and expects a user to be added
         mockMvc.perform(post("/update_account").
                         content(asJsonString(newAccount))
@@ -126,7 +126,7 @@ class ApiControllerTest {
                 .andExpect(status().isOk());
 
         Account account = accountService.findByUsername("Test24");
-        assertEquals("34", account.getActiveBookings());
+        assertEquals("123", account.getActiveBookings());
     }
 
     @Test
